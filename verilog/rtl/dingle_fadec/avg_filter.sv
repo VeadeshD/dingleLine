bdaaf31a4bdc6af56601397f1f0aeb35de879331
VeadeshD <veadesh.mail@gmail.com>
2026-07-22 22:26:58 -0400

    Update avg_filter.sv

--- verilog/rtl/dingle_fadec/avg_filter.sv
+++ verilog/rtl/dingle_fadec/avg_filter.sv
@@ -1,1 +1,29 @@
+module avg_filter (
+  input logic clk,
+  input logic rst_n,
+  input logic valid_i,
+  input logic [11:0] data_i,
 
+  output logic [11:0] data_o,
+  output logic valid_o,
+);
+
+
+//memory
+  
+//2D array of 8 12-bit numbers
+  logic [11:0] buffer_d [0:7];
+  logic [11:0] buffer_q [0:7];
+//running sum 
+  logic [14:0] sum_d, sum_q;
+
+  always_ff(posedge clk or negedge clk) begin
+    if(!rst_n) begin
+      //rest to 0
+      sum_q = 15'd0
+    
+  
+
+  
+endmodule
+  
