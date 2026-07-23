cdbaca6f03451d280f5bd47996c22a511de65a9f
VeadeshD <veadesh.mail@gmail.com>
2026-07-23 03:16:29 -0400

    Refactor sensor pipeline to connect SPI and filter

--- verilog/rtl/dingle_fadec/sensor_pipline.sv
+++ verilog/rtl/dingle_fadec/sensor_pipline.sv
@@ -16,16 +16,40 @@ module sensor_pipeline (
   //FADEC Outputs
   output logic valid_o,
   output logic [11:0] data_o
-
+);
+  
   //wire to carry data from Master to Filter
   logic raw_valid_w;
   logic [11:0] raw_data_w;
-
   spi_master master_inst (
-    valid_o,
-    raw_valid_w
+  
+    .clk        (clk),
+    .rst_n      (rst_n),
+    .start_i    (start_i),
+    .channel_i  (channel_i),
     
-    data_o,
-    raw_data_o
-);
+    // ADC Pins
+    .spi_miso_i (spi_miso_i),
+    .spi_sclk_o (spi_sclk_o),
+    .spi_cs_n_o (spi_cs_n_o),
+    .spi_mosi_o (spi_mosi_o),
+    
+    // Output to Filter (using our internal wires!)
+    .valid_o    (raw_valid_w),
+    .data_o     (raw_data_w)
+  );
+
+  avg_filter filter_inst (
+    
+    .clk        (clk),
+    .rst_n      (rst_n),
+    .valid_i    (raw_valid_w),
+    .data_i     (raw_data_w),
+    
+    // Output to Filter (using our internal wires!)
+    .valid_o    (valid_o),
+    .data_o     (data_o)    
+  );
+  
+endmodule
   
